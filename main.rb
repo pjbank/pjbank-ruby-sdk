e13b16332907795e33912e4d2ea3ec6991b1669e
lmartim <l_martim@yahoo.com.br>
2017-08-26 20:53:10 -0300

    credenciamento ok

--- main.rb
+++ main.rb
@@ -1,21 +1,72 @@
 require 'rest-client'
 
 def credenciamento_conta_digital
+    
+    puts "Nome da empresa. Ex: Exemplo Conta Digital"
+    nome_empresa = gets.chomp
+    puts "CNPJ. Ex: 60285827000110"
+    cnpj = gets.chomp
+    puts "CEP. Ex: 13032-525"
+    cep = gets.chomp
+    puts "Endereço. Ex: Rua Joaquim Vilac"
+    endereco = gets.chomp
+    puts "Número. Ex: 509"
+    numero = gets.chomp
+    puts "Bairro. Ex: Vila Teixeira"
+    bairro = gets.chomp
+    puts "Complemento. Ex: (opcional)"
+    complemento = gets.chomp
+    puts "Cidade. Ex: Campinas"
+    cidade = gets.chomp
+    puts "Estado. Ex: SP"
+    estado = gets.chomp
+    puts "DDD. Ex: 19"
+    ddd = gets.chomp
+    puts "Telefone. Ex: 987652345"
+    telefone = gets.chomp
+    puts "E-mail. Ex: api@pjbank.com.br"
+    email = gets.chomp
+    
     puts "\nLoading...\n"
-    puts RestClient.post 'https://api.pjbank.com.br/contadigital/', {nome_empresa: "Exemplo Conta Digital",  cnpj: "57471918000136",  cep: "13032-525",  endereco: "Rua Joaquim Vilac",  numero: 509,  bairro: "Vila Teixeira",  complemento: "",  cidade: "Campinas",  estado: "SP",  ddd: "19",  telefone: "987652345",  email: "api@pjbank.com.br"}
-    # response = RestClient::Request.execute(
-    #     method: :post,
-    #     url: 'https://api.pjbank.com.br/contadigital/',
-    #     body: '{"nome_empresa": "Exemplo Conta Digital",  "cnpj": "60285827000110",  "cep": "13032-525",  "endereco": "Rua Joaquim Vilac",  "numero": 509,  "bairro": "Vila Teixeira",  "complemento": "",  "cidade": "Campinas",  "estado": "SP",  "ddd": "19",  "telefone": "987652345",  "email": "api@pjbank.com.br"}'
-        
-    # )
+
+    begin
+        response = RestClient::Request.execute(
+            method: :post,
+            url: 'https://api.pjbank.com.br/contadigital/',
+            payload: {
+                nome_empresa: "#{nome_empresa}",  
+                cnpj: "#{cnpj}",  
+                cep: "#{cep}",  
+                endereco: "#{endereco}",  
+                numero: 509,  
+                bairro: "#{bairro}",  
+                complemento: "#{complemento}",  
+                cidade: "#{cidade}",  
+                estado: "#{estado}",  
+                ddd: "#{ddd}",  
+                telefone: "#{telefone}",  
+                email: "#{email}"
+            }
+        )
+
+        puts response
+    rescue RestClient::ExceptionWithResponse => err
+        puts err
+    end
 end
 
-def consulta_conta_digital(credencial, chave)
+def consulta_conta_digital
+
+    puts "Digite a Credencial. Ex: eb2af021c5e2448c343965a7a80d7d090eb64164"
+    credencial = gets.chomp
+    puts "Digite a Chave. Ex: a834d47e283dd12f50a1b3a771603ae9dfd5a32c"
+    chave = gets.chomp
+    puts "\n"
+
     puts "\nLoading...\n"
     response = RestClient::Request.execute(
         method: :get,
-        url: 'https://api.pjbank.com.br/contadigital/'+credencial,
+        url: "https://api.pjbank.com.br/contadigital/#{credencial}",
         headers: {
             'x-chave-conta': chave
         }
@@ -24,44 +75,11 @@ def consulta_conta_digital(credencial, chave)
     puts response
 end
 
-# eb2af021c5e2448c343965a7a80d7d090eb64164
-# a834d47e283dd12f50a1b3a771603ae9dfd5a32c
-
 puts "Digite sua ação:\n1 - Credenciamento de Conta Digital\n2 - Consulta Conta Digital"
 acao = gets.chomp
 case acao
     when '1' 
-        # puts "Nome da empresa. Ex: Exemplo Conta Digital"
-        # nome_empresa = gets.chomp
-        # puts "CNPJ. Ex: 60285827000110"
-        # cnpj = gets.chomp
-        # puts "CEP. Ex: 13032-525"
-        # cep = gets.chomp
-        # puts "Endereço. Ex: Rua Joaquim Vilac"
-        # endereco = gets.chomp
-        # puts "Número. Ex: 509"
-        # numero = gets.chomp
-        # puts "Bairro. Ex: Vila Teixeira"
-        # bairro = gets.chomp
-        # puts "Complemento. Ex: (opcional)"
-        # complemento = gets.chomp
-        # puts "Cidade. Ex: Campinas"
-        # cidade = gets.chomp
-        # puts "Estado. Ex: SP"
-        # estado = gets.chomp
-        # puts "DDD. Ex: 19"
-        # ddd = gets.chomp
-        # puts "Telefone. Ex: 987652345"
-        # telefone = gets.chomp
-        # puts "E-mail. Ex: api@pjbank.com.br"
-        # email = gets.chomp
         credenciamento_conta_digital
-    when '2' 
-        puts "Digite a Credencial (ex: eb2af021c5e2448c343965a7a80d7d090eb64164)"
-        credencial = gets.chomp
-        puts "\n"
-        puts "Digite a Chave (ex: a834d47e283dd12f50a1b3a771603ae9dfd5a32c)"
-        chave = gets.chomp
-        puts "\n"
-        consulta_conta_digital credencial, chave
+    when '2'
+        consulta_conta_digital
 end
