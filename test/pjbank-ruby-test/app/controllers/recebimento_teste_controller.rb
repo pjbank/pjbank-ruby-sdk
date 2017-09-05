57a0657867508008183208735bb41a50f70d2020
lmartim <l_martim@yahoo.com.br>
2017-09-04 23:17:33 -0300

    rails form recebimento boleto ok

--- test/pjbank-ruby-test/app/controllers/recebimento_teste_controller.rb
+++ test/pjbank-ruby-test/app/controllers/recebimento_teste_controller.rb
@@ -1,9 +1,11 @@
 class RecebimentoTesteController < ApplicationController
+    require 'json'
     def index
         
     end
 
     def credenciamento
+        @json = false
         @res = PJBank::Recebimento.boleto(
                     acao: :credenciamento, 
                     dados: {
@@ -17,53 +19,82 @@ class RecebimentoTesteController < ApplicationController
                         email: "#{params[:email]}"
                     }
                 )
+        if(@res != "400 Bad Request")
+            @res = ActiveSupport::JSON.decode(@res)
+            @json = true        
+        end
     end
 
     def emitir
+        @json = false
         @res = PJBank::Recebimento.boleto(
-                    acao: :credenciamento, 
+                    acao: :emitir,
+                    aut: {
+                        credencial: "#{params[:credencial]}",
+                    }, 
                     dados: {
-                        nome_empresa: "Exemplo Conta Digital",
-                        conta_repasse: "99999-9",
-                        agencia_repasse: "00001",
-                        banco_repasse: "001",
-                        cnpj: "36361126000139",  
-                        ddd: "19",  
-                        telefone: "987652345",  
-                        email: "api@pjbank.com.br"
+                        vencimento: "#{params[:vencimento]}",
+                        valor: "#{params[:valor]}",
+                        juros: "#{params[:juros]}",
+                        multa: "#{params[:multa]}",
+                        desconto: "#{params[:desconto]}",  
+                        nome_cliente: "#{params[:nome_cliente]}",  
+                        cpf_cliente: "#{params[:cpf_cliente]}",  
+                        endereco_cliente: "#{params[:endereco_cliente]}",
+                        numero_cliente: "#{params[:numero_cliente]}",
+                        complemento_cliente: "#{params[:complemento_cliente]}",
+                        bairro_cliente: "#{params[:bairro_cliente]}",
+                        cidade_cliente: "#{params[:cidade_cliente]}",
+                        estado_cliente: "#{params[:estado_cliente]}",
+                        cep_cliente: "#{params[:cep_cliente]}",
+                        logo_url: "#{params[:logo_url]}",
+                        texto: "#{params[:texto]}",
+                        grupo: "#{params[:grupo]}",
+                        pedido_numero: "#{params[:pedido_numero]}",
                     }
                 )
+
+        if(@res != "400 Bad Request")
+            @res = ActiveSupport::JSON.decode(@res)
+            @json = true        
+        end
     end
 
     def emissao
+        @json = false
         @res = PJBank::Recebimento.boleto(
-                    acao: :credenciamento, 
+                    acao: :impressaoLote,
+                    aut: {
+                        credencial: "#{params[:credencial]}",
+                        chave: "#{params[:chave]}"
+                    }, 
                     dados: {
-                        nome_empresa: "Exemplo Conta Digital",
-                        conta_repasse: "99999-9",
-                        agencia_repasse: "00001",
-                        banco_repasse: "001",
-                        cnpj: "36361126000139",  
-                        ddd: "19",  
-                        telefone: "987652345",  
-                        email: "api@pjbank.com.br"
+                        pedido_numero: "#{params[:pedido_numero]}",
+                        formato: "#{params[:formato]}",
                     }
                 )
+        if(@res != "400 Bad Request")
+            @res = ActiveSupport::JSON.decode(@res)
+            @json = true        
+        end
     end
 
     def carne
+        @json = false
         @res = PJBank::Recebimento.boleto(
-                    acao: :credenciamento, 
+                    acao: :impressaoCarne, 
+                    aut: {
+                        credencial: "#{params[:credencial]}",
+                        chave: "#{params[:chave]}"
+                    }, 
                     dados: {
-                        nome_empresa: "Exemplo Conta Digital",
-                        conta_repasse: "99999-9",
-                        agencia_repasse: "00001",
-                        banco_repasse: "001",
-                        cnpj: "36361126000139",  
-                        ddd: "19",  
-                        telefone: "987652345",  
-                        email: "api@pjbank.com.br"
+                        pedido_numero: "#{params[:pedido_numero]}",
+                        formato: "#{params[:formato]}",
                     }
                 )
+        if(@res != "400 Bad Request")
+            @res = ActiveSupport::JSON.decode(@res)
+            @json = true        
+        end
     end
 end
